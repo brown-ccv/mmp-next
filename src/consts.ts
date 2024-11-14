d1006883669b4af4e66182c528d80aa254d39045
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-11-14 12:59:32 -0500

    chore: hooks and firebase

---
+++ src/consts.ts
@@ -0,0 +1,10 @@
+export const SITE_TITLE = "MMP"
+export const SITE_DESCRIPTION = "Mesoamerican Migration Project"
+
+export const LINKS = [
+    {href: "/people", title: "People"},
+    {href: "/news", title: "News"},
+    {href: "/publications", title: "Publications"},
+    {href: "/study-design", title: "Study Design"},
+    {href: "/data", title: "Data"},
+    {href: "/documentation", title: "Documentation"},]
