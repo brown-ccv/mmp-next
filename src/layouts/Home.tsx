7454c5eea33157d88c6296ec93a4a67b6513ea5d
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-11-14 12:57:46 -0500

    feat: layouts!

---
+++ src/layouts/Home.tsx
@@ -0,0 +1,43 @@
+import {Header} from "@/components/Header"
+import {Footer} from "@/components/Footer"
+import React from "react";
+import Head from "next/head";
+
+interface HomeLayoutProps {
+  title: string
+  description: string
+  lede: string
+  children: React.ReactNode
+}
+
+export const Home: React.FC<HomeLayoutProps> = ({title, description, lede, children}) => {
+  return (
+
+      <html lang="en">
+      <Head>
+          <title>{title}</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+
+      <body>
+      <Header/>
+
+      <main>
+        <article>
+          <div>
+            <div className="flex flex-col gap-3 pb-48 md:w-1/2">
+              <h1>{title}</h1>
+              <h3>{lede}</h3>
+            </div>
+            <hr className="border-none h-0.5 bg-neutral-900 mb-16"/>
+
+            {children}
+          </div>
+        </article>
+      </main>
+      <Footer/>
+      </body>
+      </html>
+
+  )
+}
