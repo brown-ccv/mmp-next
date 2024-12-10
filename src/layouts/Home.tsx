35e535df6e9eeb6323fcd172e7986969314c3788
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-12-10 11:01:23 -0500

    feat: new logo

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,5 +1,6 @@
 import {Header} from "@/components/Header"
 import {Footer} from "@/components/Footer"
+import MmpLogo from "@/assets/mmp-logo"
 import React from "react";
 import Head from "next/head";
 
@@ -18,10 +19,15 @@ export const Home: React.FC<HomeLayoutProps> = ({title, description, lede, child
       <main>
         <article>
           <div>
-            <div className="flex flex-col gap-3 pb-48 md:w-2/3">
-              <h1>{title}</h1>
-                <h2>{description}</h2>
-              <h3>{lede}</h3>
+            <div className="flex flex-col gap-6 pb-40 md:w-2/3">
+                <div className="flex gap-6">
+                    <MmpLogo/>
+                    <div className="flex flex-col">
+                        <h1>{title}</h1>
+                        <h2>{description}</h2>
+                    </div>
+                </div>
+                <h3>{lede}</h3>
             </div>
             <hr className="border-none h-0.5 bg-neutral-900 mb-16"/>
 
