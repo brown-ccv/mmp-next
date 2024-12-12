0ad068b8a10752deaf626a93b8786b60f00b0aa7
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-12-12 10:40:48 -0500

    feat: Add TOS section to Data page

--- src/pages/data/index.js
+++ src/pages/data/index.js
@@ -11,7 +11,21 @@ export default class DataPage extends Component {
           <section className="space-y-8 readable pb-12">
             <DataContent />
           </section>
-          <DataForm />
+            <section className="pb-8">
+                <div className="space-y-8">
+                    <h2>By submitting this form, you agree with the following statements:</h2>
+                    <ul className="list-disc pl-4 space-y-2">
+                        <li>I will use the MMP data only for research and/or educational purposes
+                        </li>
+                        <li>I will not try to identify any individual, household or migrant community
+                        </li>
+                        <li>I will not give access to my copy of the MMP data to anyone who does not agree to respect
+                            these confidentiality terms
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <DataForm/>
         </Layout>)
   }
 }
