cf80f3de80487233349417ba7f1168816fce24ed
s-bessey <sam_bessey@brown.edu>
2025-03-04 10:52:05 -0500

    refactor: put MMP into subroute

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ export const SITE_TITLE = "MMP"
 export const SITE_DESCRIPTION = "Mesoamerican Migration Project"
 
 export const LINKS = [
-    {href: "/people", title: "People"},
-    {href: "/news", title: "News"},
-    {href: "/publications", title: "Publications"},
-    {href: "/study-design", title: "Study Design"},
-    {href: "/data", title: "Data"},
-    {href: "/documentation", title: "Documentation"},]
+    {href: "mmp/people", title: "People"},
+    {href: "mmp/news", title: "News"},
+    {href: "mmp/publications", title: "Publications"},
+    {href: "mmp/study-design", title: "Study Design"},
+    {href: "mmp/data", title: "Data"},
+    {href: "documentation", title: "Documentation"},]
--- src/layouts/News.tsx
+++ src/layouts/News.tsx
@@ -1,34 +1,42 @@
-import {FormattedDate} from "../components/FormattedDate"
-import {Layout} from "./Layout"
-import {NewsData} from "../lib/markdown";
+import { FormattedDate } from "../components/FormattedDate";
+import { Layout } from "./Layout";
+import { NewsData } from "../lib/markdown";
 import React from "react";
 import Image from "next/image";
 
 export interface News extends NewsData {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const News: React.FC<News> = ({title, description, heroImage, pubDate, updatedDate, children}) => {
-    const publishedDate = new Date(pubDate)
-    const updatedOnDate = updatedDate ? new Date(updatedDate) : undefined
-    if (heroImage) {
-        heroImage = heroImage.replace("/public", "")
-    }
+export const News: React.FC<News> = ({
+  title,
+  description,
+  heroImage,
+  pubDate,
+  updatedDate,
+  children,
+}) => {
+  const publishedDate = new Date(pubDate);
+  const updatedOnDate = updatedDate ? new Date(updatedDate) : undefined;
+  if (heroImage) {
+    heroImage = heroImage.replace("/public", "");
+  }
   return (
-      <Layout title={title} description={description}>
-        <article className="space-y-4">
-          {
-              heroImage && (
-                  <div>
-                    <Image className="w-full" src={heroImage} alt={title} width={200} height={200} />
-                  </div>
-              )
-          }
-          <div className="py-3 readable space-y-4">
-            {children}
+    <Layout title={title} description={description}>
+      <article className="space-y-4">
+        {heroImage && (
+          <div>
+            <Image
+              className="w-full"
+              src={heroImage}
+              alt={title}
+              width={200}
+              height={200}
+            />
           </div>
-        </article>
-      </Layout>
-
-  )
-}
+        )}
+        <div className="py-3 readable space-y-4">{children}</div>
+      </article>
+    </Layout>
+  );
+};
