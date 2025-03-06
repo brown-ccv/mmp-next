5fdfebb5a52fbced891eeaa1f9c04acc4cebeb9b
s-bessey <sam_bessey@brown.edu>
2025-03-06 11:49:03 -0500

    wip: using slugs

---
+++ src/pages/[project]/index.js
@@ -0,0 +1,82 @@
+import { GlobeIcon, Pencil1Icon, TargetIcon } from "@radix-ui/react-icons";
+import { ProjectHome } from "@/layouts/ProjectHome";
+import MiniMapSvg from "@/components/svg/MiniMaps";
+import FootPrint from "@/components/svg/FootPrint";
+import { ProjectAim } from "@/components/ProjectAim";
+import { attributes, react as HomeContent } from "@/content/pages/home.md";
+import { Component } from "react";
+import { withRouter } from "next/router";
+import { usePathname } from "next/navigation";
+
+class HomePage extends Component {
+  render() {
+    if (this.props.router.isReady) {
+      const project = this.props.router.query.project;
+      return (
+        <ProjectHome
+          title={project.toUpperCase()}
+          description={project === "mmp" ? "Mesoamerican Migration Project" : project === "lamp" ? "Latin American Migration Project" : null}
+          lede="Furthering understanding of Mexican and Central American migration to the United States"
+        >
+          <div className="hidden xl:block absolute top-24 left-4 2xl:left-12">
+            <FootPrint />
+          </div>
+          <div className="space-y-24">
+            <section className="space-y-8 readable">
+              <HomeContent />
+            </section>
+            <section className="space-y-12">
+              <h3 className="title font-semibold">Project Aims</h3>
+              <div className="flex flex-wrap gap-20 justify-center">
+                <ProjectAim
+                  color="blue"
+                  icon={<TargetIcon slot="icon" height={36} width={36} />}
+                >
+                  Collect survey data from households in migrant places of
+                  origin and in U.S. places of destination to describe and
+                  analyze the determinants and consequences of U.S. migration,
+                  including the prevalence and dynamics of new forms of
+                  migration that include women, children and families.
+                </ProjectAim>
+                <ProjectAim
+                  color="primary"
+                  icon={<GlobeIcon slot="icon" height={36} width={36} />}
+                >
+                  Broaden migration research to embrace both threat-based and
+                  opportunity-based movements, and develop and test new
+                  theoretical approaches to migration that consider threats to
+                  well-being that stem from crime, violence, and climate and
+                  weather-related events.
+                </ProjectAim>
+                <ProjectAim
+                  color="brown"
+                  icon={<Pencil1Icon height={36} width={36} />}
+                >
+                  Make the data widely accessible to researchers in the U.S. and
+                  abroad for the study of Mexican and Central American migration
+                  to the United States.
+                </ProjectAim>
+              </div>
+            </section>
+            <section className="space-y-12 readable">
+              <h3 className="title font-semibold">Where are we located?</h3>
+              <p>
+                The MMP has offices, in Mexico, at the Centro de Estudios
+                Demográficos, Urbanos y Ambientales (CEDUA) of El Colegio de
+                México in Mexico City, and in the United States, at the
+                Population Studies and Training Center (PSTC) of Brown
+                University. To contact our offices in Mexico or the United
+                States, please contact us at the following emails:
+              </p>
+              <div className="flex justify-end">
+                <MiniMapSvg />
+              </div>
+            </section>
+          </div>
+        </ProjectHome>
+      );
+    }
+  }
+}
+
+export default withRouter(HomePage);
---
+++ src/pages/[project]/news/index.js
@@ -0,0 +1,59 @@
+import { Layout } from "@/layouts/Layout";
+import { FormattedDate } from "../../../components/FormattedDate";
+import { getNews } from "@/lib/markdown";
+import { useState } from "react";
+
+export async function getStaticProps() {
+  const news = getNews();
+  return {
+    props: {
+      news: JSON.parse(JSON.stringify(news)),
+    },
+  };
+}
+
+export default function NewsPage({ news }) {
+  const [tagFilter, setTagFilter] = useState([{ value: "MMP", label: "MMP" }]);
+  const shownNews = news.filter(
+    (item) =>
+      tagFilter && tagFilter.some((tag) => item.tags.includes(tag.value))
+  );
+
+  const posts = shownNews
+    .map((item) => ({
+      ...item,
+      pubDate: new Date(item.pubDate),
+    }))
+    .sort((a, b) => b.pubDate.valueOf() - a.pubDate.valueOf());
+  return (
+    <Layout title="News" description="Recent news about MMP">
+      <ul className="flex flex-col items-start gap-6">
+        {posts.map((post) => {
+          if (post.heroImage) {
+            post.heroImage = post.heroImage.replace("/public", "");
+          }
+          return (
+            <li key={post.slug} className="flex gap-10">
+              <a
+                className="relative hidden w-80 h-72 flex-none md:block"
+                href={`/news/${post.slug}/`}
+              >
+                <img
+                  className="object-cover h-full w-full"
+                  src={post.heroImage}
+                  alt=""
+                />
+              </a>
+              <div className="grow space-y-1">
+                <a href={`/news/${post.slug}/`}>
+                  <h3 className="font-medium underline">{post.title}</h3>
+                </a>
+                <p>{post.description}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </Layout>
+  );
+}
