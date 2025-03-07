ac978064786c17075597f0e8c99dd4bbe2fcb635
s-bessey <sam_bessey@brown.edu>
2025-03-07 10:47:48 -0500

    refactor: use project in HeaderLink hrefs

--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useRouter } from "next/router";
 import React from "react";
 
 interface HeaderLinkProps {
@@ -9,10 +10,11 @@ interface HeaderLinkProps {
 }
 export const HeaderLink: React.FC<HeaderLinkProps> = ({ href, title }) => {
   const pathName = usePathname();
+  const router = useRouter();
   return (
     <Link
       className={`hover:underline underline-offset-8 text-lg ${
-        pathName === `/mmp${href}`
+        pathName === `/${router.query.project}${href}`
           ? "text-neutral-900 font-semibold underline"
           : "no-underline text-neutral-500"
       }`}
