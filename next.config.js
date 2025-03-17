aa912844567621c5b6290fed8ff7111187f2e94d
s-bessey <sam_bessey@brown.edu>
2025-03-17 09:56:22 -0400

    refactor: redirect main page to mmp project

--- next.config.js
+++ next.config.js
@@ -1,29 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push(
-            ...[
-                {
-                    test: /\.ya?ml$/,
-                    use: "js-yaml-loader",
-                },
-                {
-                    test: /\.svg$/,
-                    use: "@svgr/webpack",
-                },
-                {
-                    test: /\.md$/,
-                    loader: 'frontmatter-markdown-loader',
-                    options: { mode: ['react-component'] }
-                },
-                {
-                    test: /\.svg$/,
-                    use: ["@svgr/webpack"]
-                },
-            ]
-        );
-        return config;
-    },
+  webpack: (config) => {
+    config.module.rules.push(
+      ...[
+        {
+          test: /\.ya?ml$/,
+          use: "js-yaml-loader",
+        },
+        {
+          test: /\.svg$/,
+          use: "@svgr/webpack",
+        },
+        {
+          test: /\.md$/,
+          loader: "frontmatter-markdown-loader",
+          options: { mode: ["react-component"] },
+        },
+        {
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+        },
+      ],
+    );
+    return config;
+  },
+  async redirects() {
+    return [{ source: "/", destination: "/mmp", permanent: false }];
+  },
 };
 
 module.exports = nextConfig;
