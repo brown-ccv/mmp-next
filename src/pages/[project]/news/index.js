f5628bfd4028faaeceddacdac67e07aacd869b3d
s-bessey <sam_bessey@brown.edu>
2026-02-26 11:11:03 -0500

    refactor: associate image with title and make image decorative

--- src/pages/[project]/news/index.js
+++ src/pages/[project]/news/index.js
@@ -44,25 +44,23 @@ export default function NewsPage({ news }) {
             post.heroImage = post.heroImage.replace("/public", "");
           }
           return (
-            <li key={post.slug} className="flex gap-10">
-              <a
-                className="relative hidden w-80 h-72 flex-none md:block"
-                href={`/${project}/news/${post.slug}/`}
-              >
-                {post.altText && post.heroImage && (
-                  <img
-                    className="object-cover h-full w-full"
-                    src={post.heroImage}
-                    alt={post.altText}
-                  />
-                )}
-              </a>
-              <div className="grow space-y-1">
-                <a href={`/${project}/news/${post.slug}/`}>
+            <li key={post.slug}>
+              <a href={`/${project}/news/${post.slug}/`} className="flex gap-10 no-underline">
+                <div className="relative hidden w-80 h-72 flex-none md:block">
+                  {post.heroImage && (
+                    <img
+                      className="object-cover h-full w-full"
+                      src={post.heroImage}
+                      alt=""
+                    />
+                  )}
+                </div>
+
+                <div>
                   <h2 className="font-medium underline">{post.title}</h2>
-                </a>
-                <p>{post.description}</p>
-              </div>
+                  <p>{post.description}</p>
+                </div>
+              </a>
             </li>
           );
         })}
