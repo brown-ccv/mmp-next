06b12d8320961d39f35ca3ab26824ed798084a81
s-bessey <sam_bessey@brown.edu>
2026-03-23 15:17:59 -0400

    refactor: add accessibility reporting

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export const Footer = () => {
   const today = new Date();
   return (
-    <footer className="pt-36 pb-16">
-      &copy; {today.getFullYear()} Mesoamerican Migration Project.
+    <footer className="pt-36 pb-16 flex justify-between">
+      <div>&copy; {today.getFullYear()} Mesoamerican Migration Project.</div>
+      <Link href="https://cm.maxient.com/reportingform.php?BrownUniv=&layout_id=59">
+        Report an accessibility concern
+      </Link>
     </footer>
   );
 };
