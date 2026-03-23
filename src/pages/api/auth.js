7becc3bad5d4ae986854af442045bcc9f50c6e07
s-bessey <sam_bessey@brown.edu>
2026-03-23 10:17:40 -0400

    style: format

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -5,13 +5,13 @@ import { AuthorizationCode } from "simple-oauth2";
 const randomString = () => crypto.randomBytes(4).toString(`hex`);
 
 const handler = (req, res) => {
-  const client = create()
+  const client = create();
   const authorizationUri = client.authorizeURL({
     scope: `repo,user`,
     state: randomString(),
   });
 
-  res.redirect(authorizationUri)
+  res.redirect(authorizationUri);
 };
 
 export default handler;
--- src/pages/api/callback.js
+++ src/pages/api/callback.js
@@ -1,50 +1,28 @@
-
-
 const { create, renderBody } = require("./_lib/oauth2");
-const { client } = require("./auth.js")
+const { client } = require("./auth.js");
 
 const handler = async (req, res) => {
-  const client = create()
+  const client = create();
   const { code } = req.query;
   const options = {
-      code,
-    };
+    code,
+  };
 
   try {
-      const accessToken = await client.getToken(options);
+    const accessToken = await client.getToken(options);
 
-      console.log('The resulting token: ', accessToken.token);
+    console.log("The resulting token: ", accessToken.token);
 
-      res.status(200).send(
-        renderBody("success", {
-          token: accessToken.token.access_token,
-          provider: "github",
-        }),
-      );
-    } catch (error) {
-      console.error('Access Token Error', error.message);
-      return res.status(500).json('Authentication failed');
-    }
-  //const { host } = req.headers;
-
-
-  //   try {
-  //     // const accessToken = oauth2.authorizeURL({
-  //     //   code,
-  //     // });
-  //     const { token } = oauth2.getToken(code);
-
-  //     // res.status(200).send(
-  //     //   renderBody("success", {
-  //     //     token: token.token.access_token,
-  //     //     provider: "github",
-  //     //   }),
-  //     // );
-  //     res.status(200).json(token.token)
-
-  //   } catch (e) {
-  //     res.status(200).send(renderBody("error", e));
-  //   }
+    res.status(200).send(
+      renderBody("success", {
+        token: accessToken.token.access_token,
+        provider: "github",
+      }),
+    );
+  } catch (error) {
+    console.error("Access Token Error", error.message);
+    return res.status(500).json("Authentication failed");
+  }
 };
 
 export default handler;
