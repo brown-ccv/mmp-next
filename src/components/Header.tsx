8408dcd69274121f7d73c0d24b3bb97dea6dba64
s-bessey <sam_bessey@brown.edu>
2026-03-24 12:51:43 -0400

    fix: remove redundant layers in hamburger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,29 +14,26 @@ export const HamburgerMenu = ({ project }: Props) => {
       orientation="vertical"
       className="flex justify-end lg:hidden pt-2"
     >
-      <NavigationMenu.List className="flex justify-end">
-        <NavigationMenu.Item className="flex flex-col items-end">
-          <NavigationMenu.Trigger className="text-neutral-900 font-semibold flex justify-end">
-            <HamburgerMenuIcon width={24} height={24} aria-hidden />
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content>
-            <NavigationMenu.Sub defaultValue="sub1">
-              <NavigationMenu.List className="flex flex-col items-end space-y-1 p-2">
-                <NavigationMenu.Item>
-                  <HeaderLink href={`/${project}`}>Home</HeaderLink>
-                </NavigationMenu.Item>
-                {LINKS.map((link) => (
-                  <NavigationMenu.Item key={link.href}>
-                    <HeaderLink href={`/${project}${link.href}`}>
-                      {link.title}
-                    </HeaderLink>
-                  </NavigationMenu.Item>
-                ))}
-              </NavigationMenu.List>
-            </NavigationMenu.Sub>
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
+      <NavigationMenu.Item className="flex flex-col items-end">
+        <NavigationMenu.Trigger className="text-neutral-900 font-semibold flex justify-end">
+          <HamburgerMenuIcon width={24} height={24} aria-hidden />
+          <div className="sr-only">Navigation</div>
+        </NavigationMenu.Trigger>
+        <NavigationMenu.Content>
+          <NavigationMenu.List className="flex flex-col items-end space-y-1 p-2">
+            <NavigationMenu.Item>
+              <HeaderLink href={`/${project}`}>Home</HeaderLink>
+            </NavigationMenu.Item>
+            {LINKS.map((link) => (
+              <NavigationMenu.Item key={link.href}>
+                <HeaderLink href={`/${project}${link.href}`}>
+                  {link.title}
+                </HeaderLink>
+              </NavigationMenu.Item>
+            ))}
+          </NavigationMenu.List>
+        </NavigationMenu.Content>
+      </NavigationMenu.Item>
     </NavigationMenu.Root>
   );
 };
