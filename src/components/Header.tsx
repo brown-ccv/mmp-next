c1c4400b293c2aba1e09b81d2ca50d308c8fdfe6
s-bessey <sam_bessey@brown.edu>
2026-03-24 12:56:20 -0400

    fix: screenreader name for hamburger trigger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,26 +14,28 @@ export const HamburgerMenu = ({ project }: Props) => {
       orientation="vertical"
       className="flex justify-end lg:hidden pt-2"
     >
-      <NavigationMenu.Item className="flex flex-col items-end">
-        <NavigationMenu.Trigger className="text-neutral-900 font-semibold flex justify-end">
-          <HamburgerMenuIcon width={24} height={24} aria-hidden />
-          <div className="sr-only">Navigation</div>
-        </NavigationMenu.Trigger>
-        <NavigationMenu.Content>
-          <NavigationMenu.List className="flex flex-col items-end space-y-1 p-2">
-            <NavigationMenu.Item>
-              <HeaderLink href={`/${project}`}>Home</HeaderLink>
-            </NavigationMenu.Item>
-            {LINKS.map((link) => (
-              <NavigationMenu.Item key={link.href}>
-                <HeaderLink href={`/${project}${link.href}`}>
-                  {link.title}
-                </HeaderLink>
+      <NavigationMenu.List>
+        <NavigationMenu.Item className="flex flex-col items-end">
+          <NavigationMenu.Trigger className="text-neutral-900 font-semibold flex justify-end">
+            <HamburgerMenuIcon width={24} height={24} aria-hidden />
+            <div className="sr-only">Navigation</div>
+          </NavigationMenu.Trigger>
+          <NavigationMenu.Content>
+            <NavigationMenu.List className="flex flex-col items-end space-y-1 p-2">
+              <NavigationMenu.Item>
+                <HeaderLink href={`/${project}`}>Home</HeaderLink>
               </NavigationMenu.Item>
-            ))}
-          </NavigationMenu.List>
-        </NavigationMenu.Content>
-      </NavigationMenu.Item>
+              {LINKS.map((link) => (
+                <NavigationMenu.Item key={link.href}>
+                  <HeaderLink href={`/${project}${link.href}`}>
+                    {link.title}
+                  </HeaderLink>
+                </NavigationMenu.Item>
+              ))}
+            </NavigationMenu.List>
+          </NavigationMenu.Content>
+        </NavigationMenu.Item>
+      </NavigationMenu.List>
     </NavigationMenu.Root>
   );
 };
